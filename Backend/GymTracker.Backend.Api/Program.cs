0372203e989d1ba8aeb7b2784e3cef4196517d38
Ruslan AFK <gymgyo93@gmail.com>
2024-08-13 11:35:22 +0300

    Add Startup to API

--- Backend/GymTracker.Backend.Api/Program.cs
+++ Backend/GymTracker.Backend.Api/Program.cs
@@ -1,25 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace GymTracker.Backend.Api;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal static class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args); 
+        builder.Services.ConfigureServices();
+        builder.Build().ConfigureApp().Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ Backend/GymTracker.Backend.Api/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace GymTracker.Backend.Api;
+
+public static class Startup
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+
+    public static WebApplication ConfigureApp(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        return app;
+    }
+}
